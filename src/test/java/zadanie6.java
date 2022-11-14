bd3249093f8d906b1730afa11eeecdb4aa11347d
Jarek <jarek.malesza@gmail.com>
2022-11-14 18:08:06 +0100

    automatyczne logowanie na stronie, szukanie elementów po tagach

--- src/test/java/zadanie6.java
+++ src/test/java/zadanie6.java
@@ -1,8 +1,11 @@
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class zadanie6 {
 
@@ -11,12 +14,23 @@ public class zadanie6 {
    @BeforeClass
    public static void setUp(){
        driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
    }
     @Test
     public void zadanie6() {
        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-       driver.findElement(By.className("oxd-input oxd-input--active")).sendKeys("Admin");
+       driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+       driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+       driver.findElement(By.tagName("button")).click();
+       Assert.assertEquals("Dashboard", driver.findElement(By.tagName("h6")).getText());
+       driver.close();
+
+
+
+
     }
 
 }
+
