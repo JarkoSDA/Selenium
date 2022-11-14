0faac851e105ebd5de7cd4f8671cdec8ada936b1
Jarek <jarek.malesza@gmail.com>
2022-11-14 18:42:36 +0100

    dodanie kolejnego tesu (tego samegO),oraz BEFORE i AFTER (to znaczy uruchomić przed i po każddnym tescie)

--- src/test/java/zadanie6.java
+++ src/test/java/zadanie6.java
@@ -1,6 +1,4 @@
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,10 +7,10 @@ import java.time.Duration;
 
 public class zadanie6 {
 
-    private static WebDriver driver;
+    private WebDriver driver;
 
-   @BeforeClass
-   public static void setUp(){
+   @Before
+   public void setUp(){
        driver = new ChromeDriver();
        driver.manage().window().maximize();
        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -25,12 +23,24 @@ public class zadanie6 {
        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
        driver.findElement(By.tagName("button")).click();
        Assert.assertEquals("Dashboard", driver.findElement(By.tagName("h6")).getText());
-       driver.close();
-
-
-
+       //driver.close();
 
     }
 
+    @Test
+    public void zadanie6_1() {
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+        driver.findElement(By.tagName("button")).click();
+        Assert.assertEquals("Dashboard", driver.findElement(By.tagName("h6")).getText());
+        //driver.close();
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
 }
 
